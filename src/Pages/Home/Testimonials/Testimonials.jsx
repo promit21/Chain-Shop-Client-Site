76cbe8fea75a0c25b6dbd775fdca47d57fa59fa5
Promit Mondol <promitmondol21@gmail.com>
2024-05-19 20:56:51 +0600

    TEstimonial SEction added

---
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,51 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Rating } from "@smastrom/react-rating";
+
+import "@smastrom/react-rating/style.css";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
+
+const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <SectionTitle
+          heading={"Testimonials"}
+          subHeading={"What Our Client Say ..."}
+        ></SectionTitle>
+
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="flex flex-col gap-y-5 items-center mx-24  my-16">
+                <Rating
+                  style={{ maxWidth: 180 }}
+                  value={review.rating}
+                  readOnly
+                />
+                <h1>{review.details}</h1>
+                <p className="text-4xl text-yellow-500">{review.name}</p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
