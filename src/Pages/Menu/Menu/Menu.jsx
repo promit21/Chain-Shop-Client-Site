874f584a2826ddd4b1323d50074c497632b4b16e
Promit Mondol <promitmondol21@gmail.com>
2024-05-20 14:26:16 +0600

    Helmet Added

---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet";
+
+
+const Menu = () => {
+    return (
+        <div>
+             <Helmet><title>Chain Shop || Menu</title></Helmet>
+            
+        </div>
+    );
+};
+
+export default Menu;
--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -1,4 +1,11 @@
+import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
+  const navLinks = <>
+      <li><NavLink to='/'>Home</NavLink></li>
+      <li><NavLink to='/menu'>Menu</NavLink></li>
+  
+  </>
   return (
     <div className="navbar fixed z-20 bg-black bg-opacity-30 text-white max-w-screen-xl">
       <div className="navbar-start">
@@ -23,50 +30,14 @@ const NavBar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-blue-400 rounded-box w-56"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-black bg-opacity-30 w-full">
-                  <li>
-                    <a>Submenu kkkkkkkkk1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navLinks}
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-black bg-opacity-30 w-60">
-                <li>
-                  <a>Submenu kkkkkkkkk1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+         {navLinks}
         </ul>
       </div>
       <div className="navbar-end">
