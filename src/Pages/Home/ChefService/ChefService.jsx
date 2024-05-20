75610987795e30739cc0dde11ec9426c562af78f
Promit Mondol <promitmondol21@gmail.com>
2024-05-20 07:29:13 +0600

    Chef service css added

--- src/Pages/Home/ChefService/ChefService.jsx
+++ src/Pages/Home/ChefService/ChefService.jsx
@@ -2,7 +2,7 @@ import './ChefService.css'
 
 const ChefService = () => {
     return (
-        <div className="chef-service  h-screen flex flex-col items-center justify-center">
+        <div className="chef-service bg-fixed py-32 my-12 flex flex-col items-center">
             <div className="bg-gray-900 bg-opacity-55 p-8 w-3/4 text-center space-y-4 text-white">
                 <h1 className='text-4xl'>BISTROO BOSS</h1>
                 <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat facilis eos vitae quae quas blanditiis quasi veritatis voluptatibus, voluptas ad laborum! Reprehenderit placeat, quo minus minima expedita corporis quis dicta.</p>
