9362cdda048d8881ea937303d39084696cf6d725
Promit Mondol <promitmondol21@gmail.com>
2024-05-20 07:16:12 +0600

    ChefService added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import ChefService from "../ChefService/ChefService";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import Testimonials from "../Testimonials/Testimonials";
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
       <Banner></Banner>
       <Category></Category>
+      <ChefService></ChefService>
       <PopularMenu></PopularMenu>
       <Featured></Featured>
       <Testimonials></Testimonials>
