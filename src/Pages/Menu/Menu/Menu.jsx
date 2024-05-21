793c62e704e555eefb2335fb05eab987369c7e00
Promit Mondol <promitmondol21@gmail.com>
2024-05-21 13:09:00 +0600

    Paralux Added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,21 @@
 import { Helmet } from "react-helmet";
-
+import PageCover from "../../../Shared/PageCover/PageCover";
+import img from "../../../assets/menu/banner3.jpg"
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
     return (
         <div>
              <Helmet><title>Chain Shop || Menu</title></Helmet>
-            
+            <PageCover coverImg={img} title={"Our Menu"} details={" Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat expedita suscipit ab voluptatem ea iure non consequuntur voluptates! Incidunt numquam a enim eum quia fugiat quam odit repudiandae cumque sequi!"}></PageCover>
+            <PopularMenu></PopularMenu>
+            <PageCover coverImg={img} title={"Our Menu"} details={" Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat expedita suscipit ab voluptatem ea iure non consequuntur voluptates! Incidunt numquam a enim eum quia fugiat quam odit repudiandae cumque sequi!"}></PageCover>
+            <PopularMenu></PopularMenu>
+            <PageCover coverImg={img} title={"Our Menu"} details={" Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat expedita suscipit ab voluptatem ea iure non consequuntur voluptates! Incidunt numquam a enim eum quia fugiat quam odit repudiandae cumque sequi!"}></PageCover>
+            <PopularMenu></PopularMenu>
+            <PageCover coverImg={img} title={"Our Menu"} details={" Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat expedita suscipit ab voluptatem ea iure non consequuntur voluptates! Incidunt numquam a enim eum quia fugiat quam odit repudiandae cumque sequi!"}></PageCover>
+            <PopularMenu></PopularMenu>
+       
         </div>
     );
 };
--- src/Shared/PageCover/PageCover.jsx
+++ src/Shared/PageCover/PageCover.jsx
@@ -2,13 +2,13 @@
 const PageCover = ({ coverImg, title, details }) => {
   return (
     <div
-      className="hero min-h-screen"
+      className="hero h-[400px]"
       style={{
         backgroundImage: `url(${coverImg})`,
       }}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
+      <div className=""></div>
+      <div className="hero-content bg-black bg-opacity-60 w-3/4 text-center text-neutral-content">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">{title}</h1>
           <p className="mb-5">
