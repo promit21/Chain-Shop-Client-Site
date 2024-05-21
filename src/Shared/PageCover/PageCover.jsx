edd5432288af7f2f9504783b5381ee43acd5a780
Promit Mondol <promitmondol21@gmail.com>
2024-05-21 19:27:42 +0600

    New Feature added

--- src/Shared/PageCover/PageCover.jsx
+++ src/Shared/PageCover/PageCover.jsx
@@ -1,22 +1,24 @@
+import { Parallax } from "react-parallax";
+
 /* eslint-disable react/prop-types */
 const PageCover = ({ coverImg, title, details }) => {
   return (
-    <div
-      className="hero h-[400px]"
-      style={{
-        backgroundImage: `url(${coverImg})`,
-      }}
+    <Parallax
+      blur={{ min: -15, max: 15 }}
+      bgImage={coverImg}
+      bgImageAlt="the dog"
+      strength={-200}
     >
-      <div className=""></div>
-      <div className="hero-content bg-black bg-opacity-60 w-3/4 text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">{title}</h1>
-          <p className="mb-5">
-            {details}
-          </p>
+      <div className="hero h-[500px]">
+        <div className=""></div>
+        <div className="hero-content bg-black bg-opacity-60 w-3/4 text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+            <p className="mb-5">{details}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Parallax>
   );
 };
 
