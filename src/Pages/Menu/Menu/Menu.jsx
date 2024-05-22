fab408424c28a821c6f1750343991c9658a7c72b
Promit Mondol <promitmondol21@gmail.com>
2024-05-22 06:56:07 +0600

    Code Edited

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,23 +1,60 @@
 import { Helmet } from "react-helmet";
 import PageCover from "../../../Shared/PageCover/PageCover";
-import img from "../../../assets/menu/banner3.jpg"
-import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+import img from "../../../assets/menu/banner3.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
+import UseMenu from "../../../Hooks/UseMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
-    return (
-        <div>
-             <Helmet><title>Chain Shop || Menu</title></Helmet>
-            <PageCover coverImg={img} title={"Our Menu"} details={" Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat expedita suscipit ab voluptatem ea iure non consequuntur voluptates! Incidunt numquam a enim eum quia fugiat quam odit repudiandae cumque sequi!"}></PageCover>
-            <PopularMenu></PopularMenu>
-            <PageCover coverImg={img} title={"Our Menu"} details={" Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat expedita suscipit ab voluptatem ea iure non consequuntur voluptates! Incidunt numquam a enim eum quia fugiat quam odit repudiandae cumque sequi!"}></PageCover>
-            <PopularMenu></PopularMenu>
-            <PageCover coverImg={img} title={"Our Menu"} details={" Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat expedita suscipit ab voluptatem ea iure non consequuntur voluptates! Incidunt numquam a enim eum quia fugiat quam odit repudiandae cumque sequi!"}></PageCover>
-            <PopularMenu></PopularMenu>
-            <PageCover coverImg={img} title={"Our Menu"} details={" Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat expedita suscipit ab voluptatem ea iure non consequuntur voluptates! Incidunt numquam a enim eum quia fugiat quam odit repudiandae cumque sequi!"}></PageCover>
-            <PopularMenu></PopularMenu>
-       
-        </div>
-    );
+  const [menu] = UseMenu();
+  const salad = menu.filter((item) => item.category === "salad");
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const soup = menu.filter((item) => item.category === "soup");
+  const offered = menu.filter((item) => item.category === "offered");
+  return (
+    <div>
+      <Helmet>
+        <title>Chain Shop || Menu</title>
+      </Helmet>
+      <PageCover
+        coverImg={img}
+        title={"Our Menu"}
+        details={
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat expedita suscipit ab voluptatem ea iure non consequuntur voluptates! Incidunt numquam a enim eum quia fugiat quam odit repudiandae cumque sequi!"
+        }
+      ></PageCover>
+      <SectionTitle
+        subHeading={"Don't Miss"}
+        heading={"Today's Offered"}
+      ></SectionTitle>
+      <MenuCategory items={offered}></MenuCategory>
+      <MenuCategory
+        items={dessert}
+        title={"DESSERT"}
+        coverImg={dessertImg}
+      ></MenuCategory>
+      <MenuCategory
+        items={pizza}
+        title={"PIZZA"}
+        coverImg={pizzaImg}
+      ></MenuCategory>
+      <MenuCategory
+        items={soup}
+        title={"SOUP"}
+        coverImg={soupImg}
+      ></MenuCategory>
+      <MenuCategory
+        items={salad}
+        title={"SALAD"}
+        coverImg={saladImg}
+      ></MenuCategory>
+    </div>
+  );
 };
 
 export default Menu;
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,11 +1,18 @@
+/* eslint-disable react/prop-types */
+import MenuItem from "../../../Shared/MenuItem/MenuItem";
+import PageCover from "../../../Shared/PageCover/PageCover";
 
-
-const MenuCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const MenuCategory = ({ items, coverImg, title }) => {
+  return (
+    <div className="my-10">
+      { title && <PageCover coverImg={coverImg} title={title}></PageCover>}
+      <div className="grid lg:grid-cols-2 gap-8 mt-5">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;
